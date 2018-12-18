f4157ab10bcc03ccb0446956b3b640e55b6b6725
b3n10 <francisben.lleve@gmail.com>
2018-12-18 09:39:09 +0800

    Screencasting with audio from mic

--- screencast.sh
+++ screencast.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# https://wiki.archlinux.org/index.php/FFmpeg#Screen_capture
 ffmpeg -f x11grab -video_size 1366x768 -framerate 25 -i :0 -f alsa -i default -c:v libx264 -preset ultrafast -c:a aac /dev/shm/$( date +'%m%d%Y-%H%M%S' ).mp4
 
