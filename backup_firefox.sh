464ca92923867318e66b5bf4b2a81b79c4fe08b6
b3n10 <francisben.lleve@gmail.com>
2019-01-13 06:41:28 +0800

    Filenames for work and personal profile

--- backup_firefox.sh
+++ backup_firefox.sh
@@ -3,5 +3,8 @@
 # backup firefox profile
 # any .default profile in firefox directory
 
-fileName=/dev/shm/ffx-$( date +'%m%d%Y-%H%M%S' ).tar.gz
-tar -zcvf $fileName ~/.mozilla/firefox/*.default/ && echo "Done archiving: $fileName"
+fileName1=/dev/shm/ffx-work-$( date +'%m%d%Y-%H%M%S' ).tar.gz
+tar -zcvf $fileName1 ~/.mozilla/firefox/*.default/ && echo "Done archiving: $fileName1"
+
+fileName2=/dev/shm/ffx-personal-$( date +'%m%d%Y-%H%M%S' ).tar.gz
+tar -zcvf $fileName2 ~/.mozilla/firefox/*.personal/ && echo "Done archiving: $fileName2"
