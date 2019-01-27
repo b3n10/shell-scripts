cd373e506e529f83a350d634fea7f13ef7465d71
b3n10 <francisben.lleve@gmail.com>
2019-01-27 13:45:56 +0800

    Add support for goalsarena

--- matchat.js
+++ matchat.js
@@ -1,6 +1,10 @@
 if (yds = document.querySelector('.acp_content')) url = yds.children[0].src.slice(8);
-else url = document.querySelectorAll('.contentVencher')[0].src.slice(8);
+if (document.querySelectorAll('.videodetailsinmainarea iframe')[0]) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8);
+if (document.querySelectorAll('.contentVencher')[0]) url = document.querySelectorAll('.contentVencher')[0].src.slice(8);
 window.open('http://' + url)
 
 // browser bookmark
-javascript:(function(){if (yds = document.querySelector('.acp_content')) url = yds.children[0].src.slice(8); else url = document.querySelectorAll('.contentVencher')[0].src.slice(8);window.open('http://' + url)})()
+// with goalsarena
+javascript:(function() { if (yds = document.querySelector('.acp_content')) url = yds.children[0].src.slice(8); if (document.querySelectorAll('.videodetailsinmainarea iframe')[0]) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8); if (document.querySelectorAll('.contentVencher')[0]) url = document.querySelectorAll('.contentVencher')[0].src.slice(8); window.open('http://' + url); })()
+
+// javascript:(function(){if (yds = document.querySelector('.acp_content')) url = yds.children[0].src.slice(8); else if (document.querySelectorAll('.videodetailsinmainarea iframe')[0].src) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8); else url = document.querySelectorAll('.contentVencher')[0].src.slice(8);window.open('http://' + url)})()
