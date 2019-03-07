4a0a9ae9f37a070d46f507ffe69d5fc470f19e79
b3n10 <francisben.lleve@gmail.com>
2019-03-07 12:31:50 +0800

    saving link for reading PDF in firefox

---
+++ open-link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+link=`cat ~/.save-linked`
+
+firefox -p work "$link"
---
+++ save-link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Link: " link
+
+if [ -z $link ]; then
+    echo "Usage: save-link <URL>"
+else
+    echo $link >> ~/.save-linked
+fi
