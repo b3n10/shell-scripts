1ffdd3a6fce8d9dbd63b0bfa563276068f7e3590
b3n10 <francisben.lleve@gmail.com>
2019-04-09 06:09:23 +0800

    Update matchat, add shortcut link to gmail and monday

--- matchat.js
+++ matchat.js
@@ -5,7 +5,7 @@ window.open('http://' + url)
 
 // browser bookmark
 // with goalsarena
-javascript:(function() { if (document.querySelector('.acp_content')[0]) url = document.querySelector('.acp_content')[0].src.slice(8); if (document.querySelectorAll('.videodetailsinmainarea iframe')[0]) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8); if (document.querySelectorAll('.contentVencher')[0]) url = document.querySelectorAll('.contentVencher')[0].src.slice(8); window.open('http://' + url); })()
+javascript:(function() { if (document.querySelector('.acp_content')) url = document.querySelector('.acp_content')[0].src.slice(8); if (document.querySelectorAll('.videodetailsinmainarea iframe')[0]) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8); if (document.querySelectorAll('.contentVencher')) url = document.querySelectorAll('.contentVencher')[0].src.slice(8); window.open('http://' + url); })()
 
 // javascript:(function() { if (yds = document.querySelector('.acp_content')) url = yds.children[0].src.slice(8); if (document.querySelectorAll('.videodetailsinmainarea iframe')[0]) url = document.querySelectorAll('.videodetailsinmainarea iframe')[0].src.slice(8); if (document.querySelectorAll('.contentVencher')[0]) url = document.querySelectorAll('.contentVencher')[0].src.slice(8); window.open('http://' + url); })()
 
