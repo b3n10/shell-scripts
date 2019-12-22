bbe9883154120f42992a15c461f4f725714b0847
b3n10 <francisben.lleve@gmail.com>
2019-12-22 11:57:10 +0800

    New scripts

--- dev-php.sh
+++ dev-php.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-    echo "Usage: dev.php <directory-name>"
-else
-    cd ~/workspace/php/$1
+cd ~/workspace/php/
 
-    tmux -f ~/.dotfiles/tmux.conf new -s "php" -d "bash"
-    tmux rename-window 'server'
+tmux -f ~/.dotfiles/tmux.conf new -s "php" -d "bash"
+tmux rename-window 'bash'
 
-    tmux new-window
-    tmux rename-window 'vim'
+tmux new-window
+tmux rename-window 'vim'
 
-    tmux new-window
-    tmux rename-window 'bash'
+tmux select-window -t 0
+tmux setw -g mode-keys vi
+tmux attach-session -d
+# tmux -f ~/.dotfiles/tmux.conf
 
-    tmux new-window
-    tmux rename-window 'git'
+#!/bin/sh
 
-    tmux select-window -t 1
-
-    tmux attach-session -d
-fi
+# if [ -z "$1" ]
+# then
+#     echo "Usage: dev.php <directory-name>"
+# else
+#     cd ~/workspace/php/$1
+#
+#     tmux -f ~/.dotfiles/tmux.conf new -s "php" -d "bash"
+#     tmux rename-window 'server'
+#
+#     tmux new-window
+#     tmux rename-window 'vim'
+#
+#     tmux new-window
+#     tmux rename-window 'bash'
+#
+#     tmux new-window
+#     tmux rename-window 'git'
+#
+#     tmux select-window -t 1
+#
+#     tmux attach-session -d
+# fi
--- idea.sh
+++ idea.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# st -e bash -c "exec nohup /usr/bin/idea > /dev/null"
-st -e bash -c "exec nohup /home/ben/.local/bin/idea-IC-183.5912.21/bin/idea.sh > /dev/null"
+# st -e bash -c "exec nohup /home/ben/.local/bin/idea-IC-183.5912.21/bin/idea.sh > /dev/null"
+st -e bash -c "exec nohup /usr/bin/idea > /dev/null"
