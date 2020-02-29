c3be39594912de11880d9b222f18f68d5c3490a5
b3n10 <francisben.lleve@gmail.com>
2020-02-29 16:13:56 +0800

    Fix bug in getting values

--- check_bat.sh
+++ check_bat.sh
@@ -4,11 +4,14 @@ state=$(upower -d | grep state | head -1)
 percentage=$(upower -d | grep percentage | head -1)
 st=(${state//:/ })
 pc=(${percentage//:/ })
-p=$pc[1]
-p=(${p//%/ })
+p=${pc[1]//%/ }
+
+# should say 'charging'
+# echo ${st[1]}
+# should say number without %
+# echo $p
 
 if [[ ${st[1]} == "charging" && "$p" -ge 95 ]]; then
     /usr/bin/paplay /home/ben/scripts/0237.wav
     touch /dev/shm/testbat
-    echo $p > /dev/shm/testbat
 fi
