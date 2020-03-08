2e0e951c26f51094e331b0ef6851e24e3549e62f
b3n10 <francisben.lleve@gmail.com>
2020-03-08 21:58:18 +0800

    Use different upower argument and sound

--- check_bat.sh
+++ check_bat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-state=$(upower -d | grep state | head -1)
-percentage=$(upower -d | grep percentage | head -1)
+state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | head -1)
+percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | head -1)
 st=(${state//:/ })
 pc=(${percentage//:/ })
 p=${pc[1]//%/ }
@@ -11,7 +11,7 @@ p=${pc[1]//%/ }
 # should say number without %
 # echo $p
 
-if [[ ${st[1]} == "charging" && "$p" -ge 95 ]]; then
-    /usr/bin/paplay /home/ben/scripts/0237.wav
+if [[ ${st[1]} == "discharging" && "$p" -le 18 ]]; then
+    /usr/bin/paplay /home/ben/scripts/system-fault.ogg
     touch /dev/shm/testbat
 fi
