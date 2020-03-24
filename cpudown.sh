4ccf0226aff57191103fddbe9043cd64669ff6c1
b3n10 <francisben.lleve@gmail.com>
2020-03-25 02:45:17 +0800

    Update clock & fan speed; gmail link

--- cpudown.sh
+++ cpudown.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 for i in 0 1 2 3 ; do
-    echo 3000000 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq ;
+    echo 2300000 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq ;
 done
 
-echo level full-speed > /proc/acpi/ibm/fan
+echo level 6 > /proc/acpi/ibm/fan
+# echo level full-speed > /proc/acpi/ibm/fan
 
 su - ben -c "echo '0' > /dev/shm/cpudown_delay"
+
+# 95MB webm rendered file takes:
+# 2:15 (2.8GHz at 76C)
+# 2:24 (2.6GHz at 72C)
+# 2:37 (2.4GHz at 69C)
+
+# 160Mb rendered file
+#
+# no config: 3:39 93C
+# 2.6, lvl 7 fan: 4:22 80C
+# 2.3, lvl 7 fan: 4:54 72C
