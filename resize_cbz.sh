99fba03831f37e15db1fde400c4e5826f4d2068e
b3n10 <francisben.lleve@gmail.com>
2020-12-14 16:19:17 +0800

    Move files to temporary directory before extracting and creating new archive (cbz)

--- resize_cbz.sh
+++ resize_cbz.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
-folder=${1:0:12}
+if [ -v "$2" ]; then
+    echo 'Author not set'
+    exit 0
+fi
 
-unzip "$1" -x
+if [ ! -d "/dev/shm/comics" ]; then
+    mkdir -p /dev/shm/comics
+fi
 
-filename=""
+cp "$1" /dev/shm/comics/
+
+cd /dev/shm/comics
+
+folder=${1:0:10}
+
+unzip "$1"
 
 for i in "$folder"/*.jpg;
 do
-    # echo "file: " "$i"
+    echo "file:" "$i"
     convert "$i" -resize 1072x1448\! "$i";
 
-    if [ -z "$filename" ]; then
-        filename="${i:2:10}".cbz
-        # echo "filename: " "$filename"
-    fi
+    # if [ -z "$filename" ]; then
+    #     filename="${i:2:10}".cbz
+    #     # echo "filename: " "$filename"
+    # fi
 done
 
-zip "$filename" "$folder"/*.jpg
+filename1="$folder".cbz
+# filename2="$folder".epub
+
+zip /tmp/"$filename1" "$folder"/*.jpg
+
+rm /dev/shm/comics/*.cbz
+
+# ebook-convert "$filename1" "$filename2"
+#
+# cover=`ls "$folder"/*-000.*`
+#
+# ebook-meta "$filename1" --cover="$cover"
+# ebook-meta "$filename2" --cover="$cover"
+#
+# ebook-meta "$filename2" -a "$2"
 
 # rm *.jpg
